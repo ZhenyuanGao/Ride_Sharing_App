49f5c80eb2b13241427e46a5c0a4d34cd84b49b3
kevin gao <kegao@westmont.edu>
2020-02-08 16:16:46 -0800

    Initial Commit

---
+++ Milestone_1_test3/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Milestone_1_test3
+//
+//  Created by Kevin Gao on 2/8/20.
+//  Copyright © 2020 Kevin Gao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Milestone_1_test3Tests/Milestone_1_test3Tests.swift
@@ -0,0 +1,34 @@
+//
+//  Milestone_1_test3Tests.swift
+//  Milestone_1_test3Tests
+//
+//  Created by Kevin Gao on 2/8/20.
+//  Copyright © 2020 Kevin Gao. All rights reserved.
+//
+
+import XCTest
+@testable import Milestone_1_test3
+
+class Milestone_1_test3Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
